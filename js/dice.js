6ade937a0a33be35f5824c51d577830a9028e27d
Delapouite <delapouite@gmail.com>
2013-05-21 13:07:51 +0200

    turned dice into a proper object

---
+++ js/dice.js
@@ -0,0 +1,149 @@
+/* global THREE */
+var Dice = function(type) {
+	this.type = type;
+	this.mesh = this.getMesh();
+	this.edgesMesh = this.getEdgesMesh();
+};
+
+Dice.types = {
+	4: 'Tetra',
+	8: 'Octa',
+	20: 'Icosa'
+};
+
+Dice.prototype.getGeometry = function(radius) {
+	radius = radius || 350;
+	if (Dice.types[this.type]) {
+		return new THREE[Dice.types[this.type] + 'hedronGeometry'](radius, 0);
+	}
+	return new THREE.CubeGeometry(radius, radius, radius);
+};
+
+Dice.prototype.getMesh = function() {
+	return new THREE.Mesh(
+		this.getGeometry(),
+		new THREE.MeshBasicMaterial({color: 0xFFFFFF})
+	);
+};
+
+// to avoid artifact with compound material
+Dice.prototype.getEdgesMesh = function() {
+	return new THREE.Mesh(
+		this.getGeometry(352),
+		new THREE.MeshBasicMaterial({color: 0x000000, wireframe: true, wireframeLinewidth: 18})
+	);
+};
+
+Dice.prototype.addToScene = function(scene) {
+	this.scene = scene;
+	scene.add(this.mesh);
+	scene.add(this.edgesMesh);
+	this.populateFaces();
+};
+
+Dice.prototype.populateFaces = function() {
+	var mesh = this.mesh,
+		type = this.type,
+		scene = this.scene;
+
+	mesh.geometry.faces.forEach(function(face, i) {
+		function addNumber(text) {
+			var numberGeometry = new THREE.TextGeometry(text, {
+				size: 100, height: 0, curveSegments: 1,
+				font: 'helvetiker', weight: 'bold', style: 'normal',
+				bevelThickness: 1, bevelSize: 2, bevelEnabled: false,
+				material: 0, extrudeMaterial: 0
+			});
+
+			THREE.GeometryUtils.center(numberGeometry);
+
+			var numberMesh = new THREE.Mesh(
+				numberGeometry,
+				new THREE.MeshBasicMaterial({color: 0x000000})
+			);
+			numberMesh.position.copy(face.centroid);
+
+			// lookAwayFrom
+			var v = new THREE.Vector3();
+			v.subVectors(numberMesh.position, mesh.position).add(numberMesh.position);
+
+			numberMesh.lookAt(v);
+			numberMesh.translateZ(1);
+			scene.add(numberMesh);
+			return numberMesh;
+		}
+		function addDot(x, y) {
+			var dotMesh = new THREE.Mesh(
+				new THREE.CircleGeometry(40, 40),
+				new THREE.MeshBasicMaterial({color: 0x000000})
+			);
+			dotMesh.position.copy(face.centroid);
+
+			// lookAwayFrom
+			var v = new THREE.Vector3();
+			v.subVectors(dotMesh.position, mesh.position).add(dotMesh.position);
+
+			dotMesh.lookAt(v);
+
+			scene.add(dotMesh);
+			dotMesh.translateZ(1);
+			if (x) {
+				dotMesh.translateX(x);
+			}
+			if (y) {
+				dotMesh.translateY(y);
+			}
+			return dotMesh;
+		}
+		switch (type) {
+			// 3 numbers in each corner
+			case 4:
+				var numbers = [
+					[2, 1, 3],
+					[4, 3, 1],
+					[1, 2, 4],
+					[3, 4, 2]
+				];
+				for (var j = 0; j < 3; j++) {
+					var numberMesh = addNumber(numbers[i][j]);
+					if (i < 2) {
+						numberMesh.rotation.z += Math.PI;
+					}
+					numberMesh.rotation.z += j * 2 / 3 * Math.PI;
+					numberMesh.translateY(200);
+				}
+				break;
+			// dots
+			case 6:
+				switch(i) {
+					// 6, fallback without middle dot
+					case 0:
+						addDot(null, 100);
+						addDot(null, -100);
+					case 4:
+						addDot(100, -100);
+						addDot(-100, 100);
+					case 2:
+						addDot(100, 100);
+						addDot(-100, -100);
+						break;
+					// fallback with middle dot
+					case 5:
+						addDot(100, -100);
+						addDot(-100, 100);
+					case 3:
+						addDot(100, 100);
+						addDot(-100, -100);
+					default:
+						addDot();
+						break;
+				}
+				break;
+
+			// number in the middle of the face
+			default:
+				addNumber(i + 1);
+				break;
+		}
+	});
+};
--- js/editor.js
+++ js/editor.js
@@ -1,10 +1,4 @@
-var type = 20;
-var types = {
-	4: 'Tetra',
-	8: 'Octa',
-	20: 'Icosa'
-};
-
+/* global THREE */
 var camera = new THREE.OrthographicCamera(-400, 400, 400, -400, 1, 50000);
 camera.position.z = 5000;
 camera.aspect = 1;
@@ -20,133 +14,13 @@ var controls = new THREE.OrbitControls(camera, renderer.domElement);
 controls.rotateSpeed = 0.5;
 controls.addEventListener('change', render);
 
-function getGeometry(type, radius) {
-	radius = radius || 350;
-	if (types[type]) {
-		return new THREE[types[type] + 'hedronGeometry'](radius, 0);
-	}
-	return new THREE.CubeGeometry(radius, radius, radius);
-}
-
-// to avoid artifact with compound material
-var dice = new THREE.Mesh(
-	getGeometry(type),
-	new THREE.MeshBasicMaterial({color: 0xFFFFFF})
-);
-var diceEdges = new THREE.Mesh(
-	getGeometry(type, 352),
-	new THREE.MeshBasicMaterial({color: 0x000000, wireframe: true, wireframeLinewidth: 18})
-);
-
-scene.add(dice);
-scene.add(diceEdges);
-
-// add numbers
-dice.geometry.faces.forEach(function(face, i) {
-	function addNumber(text) {
-		var numberGeometry = new THREE.TextGeometry(text, {
-			size: 100, height: 0, curveSegments: 1,
-			font: 'helvetiker', weight: 'bold', style: 'normal',
-			bevelThickness: 1, bevelSize: 2, bevelEnabled: false,
-			material: 0, extrudeMaterial: 0
-		});
-
-		THREE.GeometryUtils.center(numberGeometry);
-
-		var numberMesh = new THREE.Mesh(
-			numberGeometry,
-			new THREE.MeshBasicMaterial({color: 0x000000})
-		);
-		numberMesh.position.copy(face.centroid);
-
-		// lookAwayFrom
-		var v = new THREE.Vector3();
-		v.subVectors(numberMesh.position, dice.position).add(numberMesh.position);
-
-		numberMesh.lookAt(v);
-		numberMesh.translateZ(1);
-		scene.add(numberMesh);
-		return numberMesh;
-	}
-	function addDot(x, y) {
-		var dotMesh = new THREE.Mesh(
-			new THREE.CircleGeometry(40, 40),
-			new THREE.MeshBasicMaterial({color: 0x000000})
-		);
-		dotMesh.position.copy(face.centroid);
-
-		// lookAwayFrom
-		var v = new THREE.Vector3();
-		v.subVectors(dotMesh.position, dice.position).add(dotMesh.position);
-
-		dotMesh.lookAt(v);
-
-		scene.add(dotMesh);
-		dotMesh.translateZ(1);
-		if (x) {
-			dotMesh.translateX(x);
-		}
-		if (y) {
-			dotMesh.translateY(y);
-		}
-		return dotMesh;
-	}
-	switch (type) {
-		// 3 numbers in each corner
-		case 4:
-			var numbers = [
-				[2, 1, 3],
-				[4, 3, 1],
-				[1, 2, 4],
-				[3, 4, 2]
-			];
-			for (var j = 0; j < 3; j++) {
-				var numberMesh = addNumber(numbers[i][j]);
-				if (i < 2) {
-					numberMesh.rotation.z += Math.PI;
-				}
-				numberMesh.rotation.z += j * 2 / 3 * Math.PI;
-				numberMesh.translateY(200);
-			}
-			break;
-		// dots
-		case 6:
-			switch(i) {
-				// 6, fallback without middle dot
-				case 0:
-					addDot(null, 100);
-					addDot(null, -100);
-				case 4:
-					addDot(100, -100);
-					addDot(-100, 100);
-				case 2:
-					addDot(100, 100);
-					addDot(-100, -100);
-					break;
-				// fallback with middle dot
-				case 5:
-					addDot(100, -100);
-					addDot(-100, 100);
-				case 3:
-					addDot(100, 100);
-					addDot(-100, -100);
-				default:
-					addDot();
-					break;
-			}
-			break;
-
-		// number in the middle of the face
-		default:
-			addNumber(i + 1);
-			break;
-	}
-});
+var dice = new Dice(20);
+dice.addToScene(scene);
 
 animate();
 
 function animate() {
-	requestAnimationFrame(animate);
+	window.requestAnimationFrame(animate);
 	controls.update();
 }
 
@@ -158,8 +32,8 @@ function getAngle() {
 	return eval(document.getElementById('angle').value) * Math.PI;
 }
 
-var paper = Raphael('raphaelContainer', 512, 512);
-paper.setViewBox(-256, -256, 512, 512);
+//var paper = Raphael('raphaelContainer', 512, 512);
+//paper.setViewBox(-256, -256, 512, 512);
 
 // HTML Controls
 
